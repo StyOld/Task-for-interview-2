37f680a33d1f962203fc50d18bfc7fc3f19264aa
Vadim Semenov <gosusty@mail.ru>
2018-12-21 00:06:57 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -9,37 +9,44 @@ export default class App extends React.Component {
         this.state = {
             imageList: [],
             sliderValue:0,
-            refreshStatus: false
+            refreshStatus: false,
+            isLoading: false
         };
     };
 
     getImages = () => {
-        this.setState({
-            imageList: []
-        });
-
-        const link='https://www.reddit.com/r/aww.json';
-
-        fetch(link)
-            .then(response => {
-                return response.json();
-            })
-            .then(result => {
+        this.setState(({
+            isLoading: true
+        }), () => {
+            fetch('https://www.reddit.com/r/aww.json')
+                .then(response => {
+                    return response.json();
+                })
+                .then(result => {
                     this.setState({
-                        imageList: result.data.children
+                        imageList: result.data.children,
+                        isLoading: false
                     })
-                }
-            )
+                })
+        });
     };
 
     autoRefresh = () => {
-        !this.state.refreshStatus ? (
-            this.timerID = setInterval(this.getImages, 3000)
-        ) : (clearInterval(this.timerID));
-
         this.setState(prevState => ({
             refreshStatus: !prevState.refreshStatus
-        }))
+        }), () => {
+            this.state.refreshStatus ? (
+                this.timerID = setInterval(this.getImages, 3000)
+            ) : (clearInterval(this.timerID));
+        })
+
+        // !this.state.refreshStatus ? (
+        //     this.timerID = setInterval(this.getImages, 3000)
+        // ) : (clearInterval(this.timerID));
+        //
+        // this.setState(prevState => ({
+        //     refreshStatus: !prevState.refreshStatus
+        // }))
     };
 
     onSliderChange = (value) => {
@@ -53,12 +60,12 @@ export default class App extends React.Component {
     };
 
     render() {
-        const {imageList, sliderValue}=this.state;
+        const {imageList, sliderValue, isLoading}=this.state;
 
         return (
             <div className='container'>
                 <Header autoRefresh={this.autoRefresh} onSliderChange={this.onSliderChange} sliderValue={sliderValue}/>
-                <ImageList imageList={imageList} sliderValue={sliderValue}/>
+                {!isLoading ? (<ImageList imageList={imageList} sliderValue={sliderValue}/>) : (<h2><b>Loading...</b></h2>)}
             </div>
         )
     }
--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -8,19 +8,16 @@ export default class ImageList extends React.Component {
         const {imageList, sliderValue}=this.props;
         const sortImages = _.sortBy(imageList, ['data.num_comments']).reverse().filter(item => item.data.num_comments>=sliderValue);
 
-        return imageList.length!==0 ? (
-                sortImages.length!==0 ? (
-                    <div className='row mt-4'>
-                        {sortImages.map(image => (
-                            <div className='col-4 mb-2' key={image.data.id}>
-                                <ImageItem item={image}/>
-                            </div>
-                        ))}
+        return sortImages.length!==0 ? (
+            <div className='row mt-4'>
+                {sortImages.map(image => (
+                    <div className='col-4 mb-2' key={image.data.id}>
+                        <ImageItem item={image}/>
                     </div>
-                ) : (
-                    <h2><b>No results found matching your criteria</b></h2>
-                )) : (
-                    <h2><b>Loading...</b></h2>
+                ))}
+                </div>
+        ) : (
+            <h2><b>No results found matching your criteria</b></h2>
         )
     }
 }
